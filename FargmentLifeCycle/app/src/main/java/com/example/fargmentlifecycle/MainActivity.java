089b7e6497abd973016bf7c15cc7243009f4c9ca
manogya69 <manogya57@gmail.com>
2023-04-18 09:39:35 +0545

    override onCreateView

--- FargmentLifeCycle/app/src/main/java/com/example/fargmentlifecycle/MainActivity.java
+++ FargmentLifeCycle/app/src/main/java/com/example/fargmentlifecycle/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.fargmentlifecycle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
+        return super.onCreateView(parent, name, context, attrs);
     }
 }
