d95fad8d7846c0ed6fdf8aa97ab28eaa01c6fee9
manogya69 <manogya57@gmail.com>
2023-04-18 10:30:42 +0545

    used fragment manager to add the fragments in the queue

--- FargmentLifeCycle/app/src/main/java/com/example/fargmentlifecycle/MainActivity.java
+++ FargmentLifeCycle/app/src/main/java/com/example/fargmentlifecycle/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.fargmentlifecycle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.os.Bundle;
@@ -21,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public View onCreateView(@Nullable View parent, @NonNull String name, @NonNull Context context, @NonNull AttributeSet attrs) {
         return super.onCreateView(parent, name, context, attrs);
+    }
+
+    public void BlueButtonClicked(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_section, BlueFragment.class, null);
+        fragmentTransaction.addToBackStack("Homepage");
+        fragmentTransaction.commit();
+    }
+
+    public void PinkButtonClicked(View view) {
+//        getSupportFragmentManager() FragmentManager
+//                .beginTransaction() FragmentTransaction
+//                .replace(R.id.fragment_section, PinkFragment.class, null)
+//                .addToBackStack("Homepage").commit();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_section, PinkFragment.class, null);
+        fragmentTransaction.addToBackStack("Homepage");
+        fragmentTransaction.commit();
     }
 }
